eb6015191b7b1184ffcbf4784483a5efa9d9fa9e
Megha Modi <meghamodi007@gmail.com>
2017-04-29 02:57:40 +0530

    Using the Django CRUD operations

---
+++ Project/servers/migrations/0001_initial.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Server',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=200)),
+                ('ip', models.GenericIPAddressField()),
+                ('order', models.IntegerField()),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
